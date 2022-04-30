a66a735655e1a8a36edb1dce27ce3d1fa7ffe7e7
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-04-29 22:30:09 -0300

    update

--- projects/web-public/pages/index.tsx
+++ projects/web-public/pages/index.tsx
@@ -2,14 +2,14 @@ import { sum } from '@alura/utils';
 import { Text } from '@alura/design-system/components/Text';
 
 export default function HomeScreen() {
-  return (
-    <div>
-      <Text tag="h2">
-        Home Screen do Projeto :)
-      </Text>
-      <Text tag="p">
-        Resultado da soma do commons: {sum(2, 2)}
-      </Text>
-    </div>
-  );
+return (
+<div>
+<Text tag="h2">
+Home Screen do Projeto :)
+</Text>
+<Text tag="p">
+Resultado da soma do commons: {sum(2, 2)}
+</Text>
+</div>
+);
 }
