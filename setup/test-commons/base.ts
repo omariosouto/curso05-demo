250e45c476628f8826cf4f92cc189596a250f742
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-04-30 00:23:28 -0300

    update tests

--- setup/test-commons/base.ts
+++ setup/test-commons/base.ts
@@ -1,9 +1,19 @@
 import type {Config} from '@jest/types';
+import path from 'path'; 
 
 // Sync object
 const config: Config.InitialOptions = {
   testEnvironment: 'jsdom',
   preset: 'ts-jest',
+  setupFilesAfterEnv: [
+    path.resolve(__dirname, 'setupTests.js')
+],
   verbose: true,
+  transform: {
+    '\\.[jt]s?$': 'ts-jest',
+    '\\.[jt]sx?$': ['babel-jest', {
+      configFile: path.resolve(__dirname, '.babelrc'),
+    }],
+  }
 };
 export default config;
