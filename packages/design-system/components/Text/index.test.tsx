dd8bd07b7a64adce191e32b19f1f2d54c071d4a7
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-04-30 00:30:50 -0300

    update

---
+++ packages/design-system/components/Text/index.test.tsx
@@ -0,0 +1,11 @@
+import { customRender } from '@alura/test-commons/react-testing-library';
+import { Text } from "./index";
+
+const render = customRender();
+
+describe("expect home", () => {
+  it("expect home", () => {
+    const { container } = render(<Text tag='h1'>Sample text</Text>);
+    expect(container).toMatchSnapshot();
+  });
+});
