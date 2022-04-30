ef6e88977c08c9bf8ba1d03545eb726949f39b44
Mario Souto <13791385+omariosouto@users.noreply.github.com>
2022-04-29 23:28:48 -0300

    update

---
+++ projects/web-public/screens/HomeScreen/HomeScreen.test.ts
@@ -0,0 +1,7 @@
+import { testValue } from "./logic";
+
+describe("expect home", () => {
+  it("expect home", () => {
+    expect(false).toBe(testValue);
+  });
+});
--- setup/eslint-commons/base.js
+++ setup/eslint-commons/base.js
@@ -2,8 +2,12 @@ module.exports = {
   extends: [
     "next",
     "eslint:recommended",
+    "jest",
     "plugin:prettier/recommended"
   ],
+  env: {
+    "jest/globals": true
+  },
   rules: {
     "no-console": "error",
   },
